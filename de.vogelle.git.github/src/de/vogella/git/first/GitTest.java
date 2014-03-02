f6a0f5398cbae3d4eb8f29ea41d8a8fcbd454e29
trifunovic3 <trifunovic.stefan@hotmail.com>
2014-03-02 11:49:14 +0100

    initial commit

---
+++ de.vogelle.git.github/src/de/vogella/git/first/GitTest.java
@@ -0,0 +1,13 @@
+package de.vogella.git.first;
+
+public class GitTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Git is geil");
+		System.out.println("Ich, Fred führe Änderungen durch");
+	}
+
+}
